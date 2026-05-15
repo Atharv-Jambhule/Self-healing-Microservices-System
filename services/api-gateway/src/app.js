e2699e60a67dd1ab00e927a1e4dfc0357b73a8ef
Atharv Jambhule <atharvjambhule7@gmail.com>
2026-05-15 17:13:00 +0530

    Implemented API gateway with auth-service routing

---
+++ services/api-gateway/src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const {
+  createProxyMiddleware,
+} = require("http-proxy-middleware");
+
+const promClient = require("prom-client");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+promClient.collectDefaultMetrics();
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "http://auth-service:3001",
+    changeOrigin: true,
+    proxyTimeout: 5000,
+    pathRewrite: {
+      "^/api/auth": "/api/auth",
+    },
+  })
+);
+
+app.get("/metrics", async (req, res) => {
+  res.set(
+    "Content-Type",
+    promClient.register.contentType
+  );
+
+  res.end(await promClient.register.metrics());
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    `🚀 API Gateway running on port ${PORT}`
+  );
+});
